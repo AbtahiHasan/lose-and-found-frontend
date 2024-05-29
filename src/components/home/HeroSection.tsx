45484bed12261e41923e37d3e63b96ef7a527c37
AbtahiHasan <anikidpersonal@gmail.com>
2024-05-29 17:10:23 +0600

    navbar improve done

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,32 +1,41 @@
 import Link from "next/link";
+import loseAndFound from "../../../public/lose-and-found.png";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <section className="relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Find What You Lost, Return What You Found
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            Lost Something? We’ll Help You Find It. Found Something? Help
-            Someone Reclaim It.
-          </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link
-              href="/"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Report a Lost Item
-            </Link>
-            <Link
-              href="#"
-              className="rounded-md border-indigo-500 border px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Report a Found Item <span aria-hidden="true">→</span>
-            </Link>
+    <section className="flex justify-center items-center h-[calc(100vh-64px)]">
+      <div className="relative isolate px-6  lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+              Find What You Lost, Return What You Found
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Lost Something? We’ll Help You Find It. Found Something? Help
+              Someone Reclaim It.
+            </p>
+            <div className="mt-10 flex flex-col gap-5 md:flex-row items-center justify-center gap-x-6">
+              <Link
+                href="/"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Report a Lost Item
+              </Link>
+              <Link
+                href="#"
+                className="rounded-md border-indigo-500 border px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Report a Found Item <span aria-hidden="true">→</span>
+              </Link>
+            </div>
           </div>
         </div>
+        <Image
+          className="absolute inset-0 -z-10 h-full w-full object-cover opacity-5"
+          src={loseAndFound}
+          alt=""
+        />
       </div>
     </section>
   );
